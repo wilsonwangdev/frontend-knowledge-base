9f476db2c15ea373ac55720a2de7bef7a3311127
wilsonwangme <scorpioaoe@gmail.com>
2025-10-09 10:40:19 +0800

    chore(config): Disable static export for Next.js project
    - Commented out `output: 'export'` in next.config.mjs
    - Temporarily disabling static site generation
    - Allows for more dynamic rendering during development

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  output: 'export',
+  // output: 'export',
 };
 
 export default withMDX(config);
